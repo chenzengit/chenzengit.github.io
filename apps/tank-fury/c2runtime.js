4f3b42fc74c387cbc44112b43273b7f85045d359
chenzengit <ikhoata@gmail.com>
2020-04-28 13:46:06 +0700

    c

--- apps/tank-fury/c2runtime.js
+++ apps/tank-fury/c2runtime.js
@@ -25988,42 +25988,45 @@ cr.plugins_.vooxe = function(runtime)
 		var self = this;
 		this.vooxe["onInit"] = function (data)
 		{
-			cr.logexport(data.Msg);
-			self.isShowingBannerAd = false;
-			self.runtime.trigger(cr.plugins_.vooxe.prototype.cnds.onInit, self);
+			// cr.logexport(data.Msg);
+			// self.isShowingBannerAd = false;
+			// self.runtime.trigger(cr.plugins_.vooxe.prototype.cnds.onInit, self);
 		};
 		this.vooxe["onError"] = function (data)
 		{
-			cr.logexport("Vooxe Googleads Plugin onError: "+data);
-			self.isShowingBannerAd = true;
-			self.runtime.trigger(cr.plugins_.vooxe.prototype.cnds.onError, self);
+			// cr.logexport("Vooxe Googleads Plugin onError: "+data);
+			// self.isShowingBannerAd = true;
+			// self.runtime.trigger(cr.plugins_.vooxe.prototype.cnds.onError, self);
 		};
 		this.vooxe["onResumeGame"] = function ()
 		{
-			cr.logexport("Vooxe Googleads Plugin: onResume");
-			self.isShowingBannerAd = false;
-			self.runtime.trigger(cr.plugins_.vooxe.prototype.cnds.onResumeGame, self);
+			//kkk
+			// cr.logexport("Vooxe Googleads Plugin: onResume");
+			// self.isShowingBannerAd = false;
+			// self.runtime.trigger(cr.plugins_.vooxe.prototype.cnds.onResumeGame, self);
 		};
 		this.vooxe["onPauseGame"] = function ()
 		{
-			cr.logexport("Vooxe Googleads Plugin: onPauseGame");
-			self.isShowingBannerAd = true;
-			self.runtime.trigger(cr.plugins_.vooxe.prototype.cnds.onPauseGame, self);
+			//kkk
+			// cr.logexport("Vooxe Googleads Plugin: onPauseGame");
+			// self.isShowingBannerAd = true;
+			// self.runtime.trigger(cr.plugins_.vooxe.prototype.cnds.onPauseGame, self);
 		};
 		this.vooxe["InitAds"] = function ()
 		{
-			var settings = {
-				gameId: self.properties[0],
-				userId: self.properties[1],
-				resumeGame: self.vooxe.onResumeGame,
-				pauseGame: self.vooxe.onPauseGame,
-				onInit: self.vooxe.onInit,
-				onError: self.vooxe.onError
-			};
-			(function(i,s,o,g,r,a,m){
-				i['GameDistribution']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)};i[r].l=1*new Date();a=s.createElement(o);m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a, m);
-			})(window, document, 'script', '//html5.api.gamedistribution.com/libs/gd/api.js', 'gdApi');
-			gdApi(settings);
+			//kkk
+			// var settings = {
+			// 	gameId: self.properties[0],
+			// 	userId: self.properties[1],
+			// 	resumeGame: self.vooxe.onResumeGame,
+			// 	pauseGame: self.vooxe.onPauseGame,
+			// 	onInit: self.vooxe.onInit,
+			// 	onError: self.vooxe.onError
+			// };
+			// (function(i,s,o,g,r,a,m){
+			// 	i['GameDistribution']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)};i[r].l=1*new Date();a=s.createElement(o);m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a, m);
+			// })(window, document, 'script', '//html5.api.gamedistribution.com/libs/gd/api.js', 'gdApi');
+			// gdApi(settings);
 		}
 	};
 	function Cnds() {};
@@ -26051,43 +26054,48 @@ cr.plugins_.vooxe = function(runtime)
 	function Acts() {};
 	Acts.prototype.ShowBanner = function (key)
 	{
-		if (!isSupported) return;
-		if (typeof (gdApi.showBanner) === "undefined")
-		{
-			cr.logexport("Vooxe Googleads Plugin is not initiliazed or AdBlocker");
-			this.vooxe["onResumeGame"]();
-			return;
-		}
-		gdApi.showBanner("{_key:"+key+"}");
-		cr.logexport("ShowBanner Key: "+key);
-		this.isShowingBannerAd = true;
+		
+			//kkk
+		// if (!isSupported) return;
+		// if (typeof (gdApi.showBanner) === "undefined")
+		// {
+		// 	cr.logexport("Vooxe Googleads Plugin is not initiliazed or AdBlocker");
+		// 	this.vooxe["onResumeGame"]();
+		// 	return;
+		// }
+		// gdApi.showBanner("{_key:"+key+"}");
+		// cr.logexport("ShowBanner Key: "+key);
+		// this.isShowingBannerAd = true;
 	};
 	Acts.prototype.PlayLog = function ()
 	{
-		if (!isSupported) return;
-		if (typeof (gdApi.play) === "undefined")
-		{
-			cr.logexport("Vooxe Googleads Plugin is not initiliazed.");
-			this.vooxe["onResumeGame"]();
-			return;
-		}
-		gdApi.play();
+			//kkk
+		// if (!isSupported) return;
+		// if (typeof (gdApi.play) === "undefined")
+		// {
+		// 	cr.logexport("Vooxe Googleads Plugin is not initiliazed.");
+		// 	this.vooxe["onResumeGame"]();
+		// 	return;
+		// }
+		// gdApi.play();
 	};
 	Acts.prototype.CustomLog = function (key)
 	{
-		if (!isSupported) return;
-		if (typeof (gdApi.customLog) === "undefined")
-		{
-			cr.logexport("Vooxe Googleads Plugin is not initiliazed.");
-			this.vooxe["onResumeGame"]();
-			return;
-		}
-		gdApi.customLog(key)
+			//kkk
+		// if (!isSupported) return;
+		// if (typeof (gdApi.customLog) === "undefined")
+		// {
+		// 	cr.logexport("Vooxe Googleads Plugin is not initiliazed.");
+		// 	this.vooxe["onResumeGame"]();
+		// 	return;
+		// }
+		// gdApi.customLog(key)
 	};
 	Acts.prototype.InitAds = function ()
 	{
-		if (!isSupported) return;
-		this.vooxe["InitAds"]();
+			//kkk
+		// if (!isSupported) return;
+		// this.vooxe["InitAds"]();
 	};
 	pluginProto.acts = new Acts();
 	function Exps() {};
